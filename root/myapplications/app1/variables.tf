4732a07cc64f4e98bf6347e3e3d5fcfa404ab9b6
AshaShreeDK <dlrashadk@gmail.com>
2025-03-08 23:19:55 +0530

    Update variables.tf

--- root/myapplications/app1/variables.tf
+++ root/myapplications/app1/variables.tf
@@ -6,7 +6,7 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
-  default     = "10.10.0.0/16"
+  default     = "10.10.10.0/16"  # /28 block provides 2^(32-28) = 16 IP addresses.
 }
 
 variable "availability_zones" {
@@ -18,13 +18,19 @@ variable "availability_zones" {
 variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
-  default     = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
+  default     = ["10.10.10.0/28", "10.10.10.16/28", "10.10.10.32/28"]
+    # (  10.10.10.0 - 10.10.10.15)
+    # ( 10.10.10.16 - 10.10.10.31)
+    # ( 10.10.10.32 - 10.10.10.47)
 }
 
 variable "private_subnet_cidrs" {
   description = "List of CIDR blocks for private subnets"
   type        = list(string)
-  default     = ["10.10.20.0/24", "10.10.21.0/24", "10.10.22.0/24"]
+  default     = ["10.10.10.48/28", "10.10.10.64/28", "10.10.10.80/28"]
+  #(10.10.10.48 - 10.10.10.63)
+  # (10.10.10.64 - 10.10.10.79)
+  # (10.10.10.80 - 10.10.10.95)
 }
 
 variable "ami_id" {
